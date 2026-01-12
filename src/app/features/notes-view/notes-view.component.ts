33865608bd55461c1888db2324c8f5f3b79ff45c
grafur922 <SL3103433522@163.com>
2026-01-12 22:39:44 +0800

    refactor(note-list-split): Implemented the separation of list and view interfaces using the split.js library

--- src/app/features/notes-view/notes-view.component.ts
+++ src/app/features/notes-view/notes-view.component.ts
@@ -1,16 +1,21 @@
-import { Component, viewChild } from '@angular/core';
+import { AfterViewInit, Component, viewChild } from '@angular/core';
 import { NoteListComponent } from '../note-list/note-list.component';
 import { NotesComponent } from '../notes/notes.component';
-
+import Split from 'split.js'
 @Component({
   selector: 'app-notes-view',
   imports: [NoteListComponent, NotesComponent],
   templateUrl: './notes-view.component.html',
   styleUrl: './notes-view.component.less'
 })
-export class NotesViewComponent {
+export class NotesViewComponent implements AfterViewInit{
   private notesRef = viewChild(NotesComponent);
-
+  constructor(){
+    
+  }
+  ngAfterViewInit(): void {
+    Split(['#note-list-panel','#note-editor-panel'])
+  }
   confirmExitIfDirty(): Promise<boolean> | boolean {
     const notes = this.notesRef();
     if (!notes?.confirmExitIfDirty) {
